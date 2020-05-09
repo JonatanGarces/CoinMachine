5e48370aeba20495ae6f14a2bfb08cbded81497a
Jonatan Garces Lara <jogala14@hotmail.com>
2020-05-09 15:37:02 -0500

    camnbios

---
+++ CoinMachine/Library/Money.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinMachine.Library
+{
+    internal class Money
+    {
+        private float money;
+        public Action Spent;
+        public Action Earned;
+
+        public float Wallet
+        {
+            get { return money; }
+            set { money = value; }
+        }
+
+        public void EarnMoney(float value)
+        {
+            money = money + value;
+            Earned?.Invoke();
+        }
+
+        public void SpendMoney(float value)
+        {
+            money = money + value;
+            Spent.Invoke();
+        }
+    }
+}
